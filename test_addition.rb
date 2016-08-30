6008b143ac24f18dda3ea447080659b90a979c7d
Susan Graves <susan@susangravescpa.com>
2016-08-30 10:36:05 -0400

    stage 1 of addition test

---
+++ test_addition.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative "addition.rb"
+class  Testaddition < Minitest::Test 
+	def test_1_equals_1
+		assert_equal(1, 1)
+	end
+
+
+	def test_1_plus_1
+		assert_equal(2, add(1, 1))
+	end
+end
