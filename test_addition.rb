b157f43ef749fb5991de02f17a75f20319da3774
Susan Graves <susan@susangravescpa.com>
2016-08-30 10:55:01 -0400

    adds 0 + 1 test

--- test_addition.rb
+++ test_addition.rb
@@ -5,8 +5,15 @@ class  Testaddition < Minitest::Test
 		assert_equal(1, 1)
 	end
 
-
 	def test_1_plus_1
 		assert_equal(2, add(1, 1))
+	end
+
+	def test_num1_plus_num2
+		assert_equal(2, add(1, 1))
+	end
+
+	def test_0_plus_1
+		assert_equal(1, add(0, 1))
 	end
 end
