053432351caba310b7a77d7fa70ac58933753258
Susan Graves <susan@susangravescpa.com>
2016-08-30 11:39:00 -0400

    test for subtraction and multiplication

--- test_addition.rb
+++ test_addition.rb
@@ -1,5 +1,7 @@
 require "minitest/autorun"
 require_relative "addition.rb"
+require_relative "subraction.rb"
+require_relative "multiply.rb"
 class  Testaddition < Minitest::Test 
 	def test_1_equals_1
 		assert_equal(1, 1)
@@ -20,4 +22,42 @@ class  Testaddition < Minitest::Test
 	def test_negative
 		assert_equal(2, add(5, -3))
 	end
+end
+
+class Testsubstraction < Minitest::Test
+	def test_1_equals_1
+		assert_equal(1, 1)
+	end
+
+	def test_1_sub_1
+		assert_equal(0, subtract(1, 1))
+	end
+
+	def test_2_sub_1
+		assert_equal(1, subtract(2, 1))
+	end
+
+	def test_5_sub_10
+		assert_equal(-5, subtract(5, 10))
+	end
+
+end
+
+class Testmultiply < Minitest::Test
+	def test_1_equals_1
+		assert_equal(1, 1)
+	end
+
+	def test_1_times_1
+		assert_equal(1, multiply(1, 1))
+	end
+
+	def test_2_times_1
+		assert_equal(2, multiply(2, 1))
+	end
+
+	def test_5_times_10
+		assert_equal(50, multiply(5, 10))
+	end
+
 end
